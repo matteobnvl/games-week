779a2189eb50e5da0b002a4a46ff5075b7291672
Mattéo <matteobonneval19@gmail.com>
2026-02-06 23:34:00 +0100

    feat: first map

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
-Text(
-    text='Hello World',
-    origin=(0, 0),
-    scale=2
-)
+# Génération de la map
+for z in range(15):
+    for x in range(15):
+        Entity(
+            model='cube',
+            color=color.gray,
+            collider='box',
+            position=(x, 0, z),
+            texture='white_cube'
+        )
+
+# Joueur
+player = FirstPersonController()
+
+
+# --- GESTION DES TOUCHES ---
+def input(key):
+    # Quitter le jeu avec Esc
+    if key == 'escape':
+        quit()
+
+    # Optionnel : Débloquer la souris avec 'm' pour pouvoir fermer la fenêtre manuellement
+    if key == 'm':
+        mouse.locked = not mouse.locked
+
 
 app.run()
